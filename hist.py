e3c83b2d337b39294051a65240c3b8691eb2163b
Purushottam69 <118760377+Purushottam69@users.noreply.github.com>
2022-11-28 19:37:02 +0530

    Update hist.py

--- hist.py
+++ hist.py
@@ -1,13 +1,20 @@
-from matplotlib import pyplot as plt
 import numpy as np
+import plotly.figure_factory as ff
+import streamlit as st
 
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
-# Creating dataset
-a = np.array([22, 87, 5, 43, 56,73, 55, 54, 11,20, 51, 5, 79, 31,27])
+# Group data together
+hist_data = [x1, x2, x3]
 
-# Creating histogram
-fig, ax = plt.subplots(figsize =(10, 7))
-ax.hist(a, bins = [0, 25, 50, 75, 100])
+group_labels = ['Group 1', 'Group 2', 'Group 3']
 
-# Show plot
-plt.show()
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
